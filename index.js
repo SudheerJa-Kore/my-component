d7cbab7b28159a676174dc35d615059bbf678665
SudheerJa-Kore <sudheer.jampana@kore.com>
2022-05-19 13:48:23 +0530

    Added my project

---
+++ index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, Text, Button, TouchableOpacity } from 'react-native'
+
+
+export default class MyComponet extends React.Component {
+  render() {
+    return (
+      <View>
+          <Text style={{fontSize:20}}>
+            Congrats!!! youhave Created Your first npm package     
+          </Text>      
+      </View>
+    );
+  }
+}
